bc46b659c0a51b65c1de99db3d123fffe27dd801
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-06-19 11:58:41 -0300

    feat: adiciona titulo pagina principal

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Prof Jhonatan, se não o mais brabo, um deles!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
